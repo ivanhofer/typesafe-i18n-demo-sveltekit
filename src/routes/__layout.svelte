e7f93e77d9710980a4280d521d116ee4f75de2c8
Ivan Hofer <ivan.hofer@outlook.com>
2021-11-07 13:07:52 +0100

    render welcome message

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<script context="module" lang="ts">
+	import { initI18n } from '$i18n/i18n-svelte'
+	import type { Load } from '@sveltejs/kit'
+
+	export const load: Load = async () => {
+		await initI18n()
+
+		return {}
+	}
+</script>
+
+<slot />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a>
-	to read the documentation
-</p>
+<script lang="ts">
+	import LL from '$i18n/i18n-svelte'
+</script>
+
+<h2>
+	{$LL.HI()}
+</h2>
