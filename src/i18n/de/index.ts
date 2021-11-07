5d87755a15d5cae323b9fea313aae760ed572ef6
Ivan Hofer <ivan.hofer@outlook.com>
2021-11-07 13:04:36 +0100

    setup `typesafe-i18n`

---
+++ src/i18n/de/index.ts
@@ -0,0 +1,6 @@
+import type { Translation } from '../i18n-types'
+
+const de: Translation = {
+}
+
+export default de
---
+++ src/i18n/en/index.ts
@@ -0,0 +1,7 @@
+import type { BaseTranslation } from '../i18n-types'
+
+const en: BaseTranslation = {
+	// TODO: your translations go here
+}
+
+export default en
