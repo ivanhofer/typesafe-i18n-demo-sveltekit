7deb98716f5f1bb2937db1bd79d5095bf150227c
Ivan Hofer <ivan.hofer@outlook.com>
2021-11-07 13:23:48 +0100

    add SSR support for locale routes

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,17 +1,33 @@
 <script context="module" lang="ts">
 	import { initI18n } from '$i18n/i18n-svelte'
 	import type { Load } from '@sveltejs/kit'
+	import type { Locales } from '$i18n/i18n-types'
+	import { replaceLocaleInUrl } from '../utils'
+	import { baseLocale, locales } from '$i18n/i18n-util'
 
 	export const load: Load = async ({ page, session }) => {
-		await initI18n(session.locale)
+		const lang = page.params.lang as Locales
 
 		// redirect to preferred language if user comes from page root
-		if (!page.params.lang) {
+		if (!lang) {
 			return {
 				status: 302,
 				redirect: `/${session.locale}`,
 			}
 		}
+
+		// redirect to base locale if language is not present
+		if (!locales.includes(lang)) {
+			return {
+				status: 302,
+				redirect: replaceLocaleInUrl(page.path, baseLocale),
+			}
+		}
+
+		// delete session locale since we don't need it to be sent to the client
+		delete session.locale
+
+		await initI18n(lang)
 
 		return {}
 	}
