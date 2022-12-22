589292829eda80f4aa8a719a5cc2081ffa90da20
beeb <vbersier@gmail.com>
2022-12-22 15:10:34 +0100

    feat: add component to render alternate link in <head>

---
+++ src/lib/HeadHrefLangs.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { locales } from '$i18n/i18n-util'
+	import { page } from '$app/stores'
+	import { replaceLocaleInUrl } from '../utils'
+</script>
+
+{#each locales as l}
+	<link rel="alternate" hreflang={l} href={`${replaceLocaleInUrl($page.url, l)}`} />
+{/each}
