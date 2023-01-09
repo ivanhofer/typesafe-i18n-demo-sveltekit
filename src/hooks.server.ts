33c035ee59452576dc96731f8b70180c6fabecb7
Hofer Ivan <ivan.hofer@outlook.com>
2023-01-09 08:35:46 +0100

    fix html lang attribute replacement

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -31,7 +31,7 @@ export const handle: Handle = async ({ event, resolve }) => {
 	console.info(LL.log({ fileName: 'hooks.server.ts' }))
 
 	// replace html lang attribute with correct language
-	return resolve(event, { transformPageChunk: ({ html }) => html.replace('%lang%', lang) })
+	return resolve(event, { transformPageChunk: ({ html }) => html.replace('%lang%', locale) })
 }
 
 const getPreferredLocale = ({ request }: RequestEvent) => {
