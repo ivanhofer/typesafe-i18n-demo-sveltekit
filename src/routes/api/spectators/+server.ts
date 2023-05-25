c9570e1a6bbba234f448bf9263097605f4469011
Ivan Hofer <ivan.hofer@outlok.com>
2023-05-25 07:49:47 +0200

    add missing log

--- src/routes/api/spectators/+server.ts
+++ src/routes/api/spectators/+server.ts
@@ -1,12 +1,15 @@
 import { json } from '@sveltejs/kit'
 import type { RequestHandler } from './$types'
 
-export const GET: RequestHandler = async ({ url }) => {
+export const GET: RequestHandler = async ({ url, locals: { LL } }) => {
 	// no real data, just a simulation ;)
 	const oldSpectators = Number(url.searchParams.get('oldSpectators') ?? '0')
 	let spectators = oldSpectators * 2 + 1
 	if (spectators > 100_000) {
 		spectators = 0
 	}
+
+	console.info(LL.log({ fileName: 'api/+server.ts' }))
+
 	return json({ spectators })
 }
