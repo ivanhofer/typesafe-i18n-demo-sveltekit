c45e636b786f84b513dfc60bada9dd108d45d1e0
Ivan Hofer <ivan.hofer@outlok.com>
2023-07-10 09:54:44 +0200

    add better support for `base` href

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,21 +1,23 @@
+import { base } from '$app/paths'
 import type { Locales } from '$i18n/i18n-types.js'
 import { detectLocale, i18n, isLocale } from '$i18n/i18n-util'
 import { loadAllLocales } from '$i18n/i18n-util.sync'
 import { redirect, type Handle, type RequestEvent } from '@sveltejs/kit'
 import { initAcceptLanguageHeaderDetector } from 'typesafe-i18n/detectors'
+import { getPathnameWithoutBase } from './utils.js'
 
 loadAllLocales()
 const L = i18n()
 
 export const handle: Handle = async ({ event, resolve }) => {
 	// read language slug
-	const [, lang] = event.url.pathname.split('/')
+	const [, lang] = getPathnameWithoutBase(event.url).split('/')
 
 	// redirect to base locale if no locale slug was found
 	if (!lang) {
 		const locale = getPreferredLocale(event)
 
-		throw redirect(307, `/${locale}`)
+		throw redirect(307, `${base}/${locale}`)
 	}
 
 	// if slug is not a locale, use base locale (e.g. api endpoints)
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,45 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+# typesafe-i18n demo sveltekit
+
+**Here is a [video](https://www.youtube.com/watch?v=C6O5pMMMTG0) for better context.** _SvelteKit has undergone some major changes since this video was published. For the most up-to-date way of handling i18n, please refer to the code in this repository._
+
+This example demonstrates a [`typesafe-i18n`](https://github.com/ivanhofer/typesafe-i18n)-integration with [SvelteKit](https://kit.svelte.dev/).
+
+### LIVE-DEMO: https://typesafe-i18n-demo-sveltekit.vercel.app
+
+It is a basic example and shows:
+
+-  basic i18n setup of three different locales
+-  auto-detecting user language preferences
+-  full SSR support
+-  SEO optimizations
+-  locale switching
+-  lazy loading of locales
+-  persistent locale state via language routes
+-  typesafety features of `typesafe-i18n`
+-  plural rules
+-  locale specific date-formatting
+
+What is missing:
+
+-  opinion how to localize slugs\
+   this highly depends where your data comes from. This will probably differ from project to project.
+-  cross-links between your language slugs\
+   like mentioned above, this will differ from project to project. You can find useful resources here:\
+   -  [`rel="alternate"` links](https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/rel#attr-alternate)
+   -  [`hreflang` attribute](https://developer.mozilla.org/de/docs/Web/HTML/Element/a#attr-hreflang)
+   -  [SEO optimized examples](https://developers.google.com/search/docs/advanced/crawling/localized-versions)
+
+## add `typesafe-i18n` to an existing SvelteKit project
+
+If you want to implement the solution in a existing project, you can take a look at the [commit history](https://github.com/ivanhofer/typesafe-i18n-demo-sveltekit/commits/main) to see a step-by-step guide.
+
+## JavaScript version
+
+You can find the exact same example in a JavaScript only version [here](https://github.com/ivanhofer/typesafe-i18n-demo-sveltekit-jsdoc).\
+It offers the same typesafety features but instead of TypeScript files it is written in JavaScript with JsDoc comments.
+
+## learn more
+
+If you want to know more about `typesafe-i18n` head over to the main repo:
+
+### https://github.com/ivanhofer/typesafe-i18n
