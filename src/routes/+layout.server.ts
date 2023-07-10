d01ee0cd224cf415169252c2b55972b808a8376e
Ivan Hofer <ivan.hofer@outlok.com>
2023-07-10 09:02:50 +0200

    delete old code

--- src/routes/+layout.server.ts
+++
@@ -1,8 +0,0 @@
-import type { LayoutServerLoad } from './$types'
-
-export const load: LayoutServerLoad = ({ locals: { locale, LL } }) => {
-	console.info(LL.log({ fileName: '+layout.server.ts' }))
-
-	// pass locale information from "server-context" to "shared server + client context"
-	return { locale }
-}
--- src/routes/[lang=lang]/+page.server.ts
+++
@@ -1,5 +0,0 @@
-import type { PageServerLoad } from './$types'
-
-export const load: PageServerLoad = ({ locals: { LL } }) => {
-	console.info(LL.log({ fileName: '+page.server.ts' }))
-}
